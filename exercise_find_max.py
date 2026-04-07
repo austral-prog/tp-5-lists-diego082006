a0cb0e7d44a58064491fd7b07c5b3b4bfe02e9e6
diego082006 <ddelgado1@mail.austral.edu.ar>
2026-04-07 11:38:32 -0300

    Update exercise_find_max.py

--- exercise_find_max.py
+++ exercise_find_max.py
@@ -11,4 +11,7 @@ def find_max(lista):
     Returns:
         El valor máximo de la lista o None si está vacía
     """
-    return max(lista)
+    if not lista:
+        return None
+    else:
+        return max(lista)
